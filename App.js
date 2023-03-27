2224de42c31f27d40c92b29069a4622a7775c040
vibin17604 <108916451+vibin17604@users.noreply.github.com>
2023-03-27 23:04:49 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Appbar from './Day 3/Appbar';
+import Image from './Day 3/Image';
+
+function App() {
+  return (
+    <div >
+      <Appbar/>
+      <br></br>
+      <Image/>
+    </div>
+  );
+}
+
+export default App;
