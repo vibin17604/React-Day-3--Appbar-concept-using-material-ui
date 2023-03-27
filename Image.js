974c6748b9e532a8c939f16b1a296a20c0549fa2
vibin17604 <108916451+vibin17604@users.noreply.github.com>
2023-03-27 22:48:56 +0530

    Add files via upload

---
+++ Image.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import msd from './MSD.jpg'
+
+function Image() {
+  return (
+    <div className='div1'>
+        <center>
+            <img src={msd} width="1000px" hieght="350px" alt=''></img>
+        </center>
+    </div>
+  )
+}
+
+export default Image
